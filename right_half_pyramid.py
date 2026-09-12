39e2a75b74fff6c1659c87e88f306cc4f9d74874
Munnoi <midhunbinoy123@gmail.com>
2026-09-12 06:41:19 +0530

    add v2 for right_half_pyramid - 12/9/26

--- right_half_pyramid.py
+++ right_half_pyramid.py
@@ -1,9 +1,16 @@
 def right_half_pyramid(n):
+    # Time - O(n^2), Space - O(1)
     def v1():
         for i in range(n):
             for _ in range(i + 1):
                 print("*", end="")
             print()
-    v1()
+    # v1()
+
+    # Time - O(n^2), Space - O(1)
+    def v2():
+        for i in range(1, n+1):
+            print("*" * i)
+    # v2()
 
 right_half_pyramid(5)
